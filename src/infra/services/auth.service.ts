1f1bbd39f4d62599a00570d8a30bf8f90f7ddbd8
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-06-13 09:36:12 -0300

    feat: create basic services

---
+++ src/infra/services/auth.service.ts
@@ -0,0 +1,3 @@
+// login/?v_usuario=suporte@tecmil.com.br&v_senha=3318813ee21eaf7b6f45a31965e9085f
+
+// Login: suporte@tecmil.com.br Senha: tecmil
---
+++ src/infra/services/order.service.ts
@@ -0,0 +1,9 @@
+// /pedido/?v_empresa=002&v_pedido=14824&v_item=0
+// /pedido/?v_empresa=002&v_pedido=14824&v_item=5
+
+
+// - Exemplo de pedidos:
+// - Empresa sempre a 002.
+//   - pedido: 16524 - grande com 21 itens
+//   - pedido: 14824 - médio 21 itens
+//   - pedido: 16024 - pequeno 2 itens
