3a790fc234fec47fa132f3c9f61d2b7aa69e7c7a
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-06-14 00:36:31 -0300

    fix: add header

--- src/app/(app)/inventory.tsx
+++ src/app/(app)/inventory.tsx
@@ -1,7 +1,20 @@
-import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+
+import { Header } from "@/components/common";
+import { theme } from "@/config/theme";
 
 export default function Inventory() {
     return (
-        <View></View>
+        <SafeAreaView style={styles.container}>
+            <Header title="Inventário" />
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.white,
+    },
+});
