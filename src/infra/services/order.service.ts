ae1a5ad6118db84ec27569127d993db28cb6767f
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-06-15 15:19:07 -0300

    fix: order types

--- src/infra/services/order.service.ts
+++ src/infra/services/order.service.ts
@@ -1,9 +1,73 @@
-// /pedido/?v_empresa=002&v_pedido=14824&v_item=0
-// /pedido/?v_empresa=002&v_pedido=14824&v_item=5
+import api from "../api";
 
+export type OrderItem = {
+  item: string;
+  localizacao: string;
+  codigo: string;
+  referencia: string;
+  saldo: string;
+  qtd_sep: string;
+};
 
-// - Exemplo de pedidos:
-// - Empresa sempre a 002.
-//   - pedido: 16524 - grande com 21 itens
-//   - pedido: 14824 - médio 21 itens
-//   - pedido: 16024 - pequeno 2 itens
+export type Order = {
+  id: string;
+  cliente: string;
+  entrega_endereco: string;
+  entrega_numero: string;
+  entrega_complemento: string | null;
+  entrega_bairro: string;
+  entrega_cep: string;
+  entrega_cidade: string;
+  itens: OrderItem[];
+};
+
+export type GetOrderParams = {
+  action?: string;
+  company: string;
+  item?: string;
+  orderId: string;
+};
+
+type OrderResponse = {
+  error?: string;
+  pedido?: Order;
+};
+
+export async function getOrder({
+  action,
+  company,
+  item = "0",
+  orderId,
+}: GetOrderParams) {
+  try {
+    const response = await api.get<OrderResponse>(
+      `pedido/?v_empresa=${company}&v_pedido=${orderId}&v_item=${item}&v_acao=${action}`,
+    );
+    return response;
+  } catch (error) {
+    console.log("error service: ", error);
+    throw error;
+  }
+}
+
+type GetFullOrderParams = {
+  action?: string;
+  item?: string;
+  orderId: string;
+};
+
+export async function getFullOrder({
+  orderId,
+  item = "0",
+  action,
+}: GetFullOrderParams) {
+  try {
+    const response = await api.get<OrderResponse>(
+      `pedido/?v_pedido=${orderId}&v_item=${item}&v_acao=${action}`,
+    );
+    return response;
+  } catch (error) {
+    console.log("error service: ", error);
+    throw error;
+  }
+}
