3676c8aefdfd97f9f64f11a84bed748989af58b7
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-06-15 15:35:37 -0300

    fix: lint

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,79 +1,68 @@
-import { useState } from "react";
 import { useRouter } from "expo-router";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import Audit from '@/assets/audit.svg';
-import Inventory from '@/assets/inventory.svg';
-import Picking from '@/assets/picking.svg';
-import Shipping from '@/assets/shipping.svg';
+import Audit from "@/assets/audit.svg";
+import Inventory from "@/assets/inventory.svg";
+import Picking from "@/assets/picking.svg";
+import Shipping from "@/assets/shipping.svg";
 
 import { Header, ServiceCard } from "@/components/Screens/Home";
 
 export default function Home() {
-    const [pathname, setPathname] = useState('');
+  const router = useRouter();
 
-    const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <Header />
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-            <Header />
-
-            <View style={styles.servicesContent}>
-                <View style={styles.servicesRow}>
-                    <ServiceCard
-                        icon={Picking}
-                        title="Separação"
-                        selected={pathname === 'separation'}
-                        onPress={() => {
-                            setPathname('separation');
-                            router.navigate('separation')
-                        }}
-                    />
-                    <ServiceCard
-                        icon={Shipping}
-                        title="Expedição"
-                        selected={pathname === 'expedition'}
-                        onPress={() => {
-                            setPathname('expedition');
-                            router.navigate('expedition')
-                        }}
-                    />
-                </View>
-                <View style={styles.servicesRow}>
-                    <ServiceCard
-                        icon={Inventory}
-                        title="Inventário"
-                        selected={pathname === 'inventory'}
-                        onPress={() => {
-                            setPathname('inventory');
-                            router.navigate('inventory')
-                        }}
-                    />
-                    <ServiceCard
-                        icon={Audit}
-                        title="Auditoria"
-                        selected={pathname === 'audit'}
-                        onPress={() => {
-                            setPathname('audit');
-                            router.navigate('audit')
-                        }}
-                    />
-                </View>
-            </View>
-        </SafeAreaView>
-    )
+      <View style={styles.servicesContent}>
+        <View style={styles.servicesRow}>
+          <ServiceCard
+            icon={Picking}
+            title="Separação"
+            onPress={() => {
+              router.navigate(`picking-details/${undefined}`);
+            }}
+          />
+          <ServiceCard
+            icon={Shipping}
+            title="Expedição"
+            onPress={() => {
+              router.navigate("expedition");
+            }}
+          />
+        </View>
+        <View style={styles.servicesRow}>
+          <ServiceCard
+            icon={Inventory}
+            title="Inventário"
+            onPress={() => {
+              router.navigate("inventory");
+            }}
+          />
+          <ServiceCard
+            icon={Audit}
+            title="Auditoria"
+            onPress={() => {
+              router.navigate("audit");
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    servicesContent: {
-        marginTop: 24,
-        gap: 16,
-        marginHorizontal: 20,
-    },
-    servicesRow: {
-        gap: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    }
-})
+  servicesContent: {
+    marginTop: 24,
+    gap: 16,
+    marginHorizontal: 20,
+  },
+  servicesRow: {
+    gap: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
