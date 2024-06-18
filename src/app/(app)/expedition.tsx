e02929473092e17359f0cd5324e8aaa04d994cb6
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2024-06-18 20:05:10 -0300

    fix: adjust expedition

--- src/app/(app)/expedition.tsx
+++ src/app/(app)/expedition.tsx
@@ -1,48 +1,13 @@
-import { useCallback, useState } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Toast from "react-native-toast-message";
 
-import { Header, Loading, QrCodeButton } from "@/components/common";
-import { ExpeditionDetails } from "@/components/Screens/Expedition";
+import { Header } from "@/components/common";
 import { theme } from "@/config/theme";
-import { Order, getFullOrder } from "@/infra/services/order.service";
 
 export default function Expedition() {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<Order>();
-
-  async function loadOrder(orderId: string) {
-    try {
-      setLoading(true);
-      const response = await getFullOrder({ orderId });
-      if (response.data?.error) {
-        Toast.show({
-          text1: "Erro ao buscar dados do pedido.",
-          type: "error",
-          text2: response.data?.error,
-        });
-        return;
-      }
-      setData(response.data.pedido);
-    } catch (error) {
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  const handleChangeData = useCallback((data: string) => {
-    setData(undefined);
-    loadOrder(data.replace(" ", ""));
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Expedição" />
-
-      {data ? <ExpeditionDetails data={data} /> : loading ? <Loading /> : null}
-
-      <QrCodeButton onChangeData={handleChangeData} />
     </SafeAreaView>
   );
 }
