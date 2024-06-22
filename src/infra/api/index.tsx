7392e432ffd5186154d3eff742f8145119881b8e
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-06-22 12:06:00 -0300

    fix: add api pwm

--- src/infra/api/index.tsx
+++ src/infra/api/index.tsx
@@ -3,3 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
+
+export const apiPwm = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_PWM_API_URL,
+});
